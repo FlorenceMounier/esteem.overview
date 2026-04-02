914776b6e956fae8457655083920dd4255e5b7e1
MOUNIER FLORENCE <florence.mounier@inrae.fr>
2026-04-02 14:48:03 +0200

    feat: publication M&M haline sectors

--- data-raw/1.2-plot_POMET_haline_zones.R
+++ data-raw/1.2-plot_POMET_haline_zones.R
@@ -11,7 +11,13 @@
 
 library(dplyr)
 library(magick) # crop images
-library(patchwork)
+library(cowplot)
+
+fct_img_crop <- function(path) {
+  ggdraw() +
+    draw_image(path) +
+    theme(plot.margin = margin(0, 0, 0, 0))
+}
 
 # =====================================================
 # 01. Gironde haline zones from POMET
@@ -21,15 +27,14 @@ library(patchwork)
 plot_POMET_map_gironde <- plot_estuary_map(
   data = data_POMET_densities |> filter(estuary == "Gironde"),
   estuary_name = "Gironde", colour_var = haline_zone
-)
-
-# ----- save the image -----
+) +
+  theme(legend.position = "bottom") +
+  labs(colour = "Haline sector")
 ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_gironde.jpg",
        plot = plot_POMET_map_gironde)
 
-# ----- crop & save the image -----
 img_gironde <- image_read("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_gironde.jpg")
-img_trim_gironde <- image_trim(img_gironde)
+img_trim_gironde <- image_trim(img_gironde) # crop image
 image_write(img_trim_gironde, "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_gironde.jpg")
 
 # ----- plot salinity distribution in haline zones -----
@@ -37,26 +42,32 @@ plot_POMET_salinity_zones_gironde <- ggplot(data = data_POMET_densities |> filte
   aes(x = salinite, fill = haline_zone) +
   geom_boxplot() +
   geom_vline(xintercept = 18, linewidth = 1.5) +
-  theme(legend.position = "none") +
-  coord_flip() +
-  theme(
-    axis.text.x = element_blank(),   # supprime les labels
-    axis.ticks.x = element_blank()   # supprime les ticks
-  )
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x  = element_blank(),
+        axis.ticks.x = element_blank()) +
+  ggplot2::labs(x = "Salinity (g/L)") +
+  coord_flip()
 
-# ---- Join plots in one image ----
-ggplot_POMET_haline_zones_gironde <- plot_POMET_map_gironde + plot_POMET_salinity_zones_gironde +
-  plot_layout(widths = c(7, 3), heights = c(3,1))
+ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_salinity_zones_gironde.jpg",
+       width = 7, height = 7, units = "cm", dpi = 300,
+       plot = plot_POMET_salinity_zones_gironde)
 
-# ----- save the image -----
+
+# ---- Join images with {cowplot} ----
+
+ggplot_POMET_haline_zones_gironde <- plot_grid(
+  fct_img_crop("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_gironde.jpg"),
+  fct_img_crop("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_salinity_zones_gironde.jpg"),
+  ncol = 2,
+  rel_widths = c(1.5, 1), scale = c(0.7, 1)
+)
 ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_gironde.jpg",
        plot = ggplot_POMET_haline_zones_gironde)
 
-# ----- crop the image -----
 img_gironde <- image_read("inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_gironde.jpg")
-img_trim_gironde <- image_trim(img_gironde)
+img_trim_gironde <- image_trim(img_gironde) # crop the image
 image_write(img_trim_gironde, "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_gironde.jpg")
-
 
 
 # =====================================================
@@ -67,15 +78,14 @@ image_write(img_trim_gironde, "inst/mat_meth/maps/POMET/haline_zones/ggplot_POME
 plot_POMET_map_loire <- plot_estuary_map(
   data = data_POMET_densities |> filter(estuary == "Loire"),
   estuary_name = "Loire", colour_var = haline_zone
-)
-
-# ----- save the image -----
+) +
+  theme(legend.position = "bottom") +
+  labs(colour = "Haline sector")
 ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_loire.jpg",
        plot = plot_POMET_map_loire)
 
-# ----- crop the image -----
 img_loire <- image_read("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_loire.jpg")
-img_trim_loire <- image_trim(img_loire)
+img_trim_loire <- image_trim(img_loire) # crop the image
 image_write(img_trim_loire, "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_loire.jpg")
 
 # ----- plot salinity distribution in haline zones -----
@@ -83,24 +93,31 @@ plot_POMET_salinity_zones_loire <- ggplot(data = data_POMET_densities |> filter(
   aes(x = salinite, fill = haline_zone) +
   geom_boxplot() +
   geom_vline(xintercept = 18, linewidth = 1.5) +
-  theme(legend.position = "none") +
-  coord_flip() +
-  theme(
-    axis.text.x = element_blank(),   # supprime les labels
-    axis.ticks.x = element_blank()   # supprime les ticks
-  )
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x  = element_blank(),
+        axis.ticks.x = element_blank()) +
+  ggplot2::labs(x = "Salinity (g/L)") +
+  coord_flip()
 
-# ---- Join plots in one image ----
-ggplot_POMET_haline_zones_loire <- plot_POMET_map_loire + plot_POMET_salinity_zones_loire +
-  plot_layout(widths = c(4, 1), heights = c(1, 2))
+ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_salinity_zones_loire.jpg",
+       width = 7, height = 7, units = "cm", dpi = 300,
+       plot = plot_POMET_salinity_zones_loire)
 
-# ----- save the image -----
+
+# ---- Join images with {cowplot} ----
+
+ggplot_POMET_haline_zones_loire <- plot_grid(
+  ggdraw() + draw_image("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_loire.jpg"),
+  ggdraw() + draw_image("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_salinity_zones_loire.jpg"),
+  ncol = 2,
+  rel_widths = c(1.5, 1)
+)
 ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_loire.jpg",
        plot = ggplot_POMET_haline_zones_loire)
 
-# ----- crop the image -----
 img_loire <- image_read("inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_loire.jpg")
-img_trim_loire <- image_trim(img_loire)
+img_trim_loire <- image_trim(img_loire) # crop the image
 image_write(img_trim_loire, "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_loire.jpg")
 
 
@@ -112,15 +129,15 @@ image_write(img_trim_loire, "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_
 plot_POMET_map_seine <- plot_estuary_map(
   data = data_POMET_densities |> filter(estuary == "Seine"),
   estuary_name = "Seine", colour_var = haline_zone
-)
+) +
+  theme(legend.position = "bottom") +
+  labs(colour = "Haline sector")
 
-# ----- save the image -----
 ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_seine.jpg",
        plot = plot_POMET_map_seine)
 
-# ----- crop the image -----
 img_seine <- image_read("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_seine.jpg")
-img_trim_seine <- image_trim(img_seine)
+img_trim_seine <- image_trim(img_seine) # crop the image
 image_write(img_trim_seine, "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_seine.jpg")
 
 # ----- plot salinity distribution in haline zones -----
@@ -128,23 +145,53 @@ plot_POMET_salinity_zones_seine <- ggplot(data = data_POMET_densities |> filter(
   aes(x = salinite, fill = haline_zone) +
   geom_boxplot() +
   geom_vline(xintercept = 18, linewidth = 1.5) +
-  theme(legend.position = "none") +
-  coord_flip() +
-  theme(
-    axis.text.x = element_blank(),   # supprime les labels
-    axis.ticks.x = element_blank()   # supprime les ticks
-  )
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x  = element_blank(),
+        axis.ticks.x = element_blank()) +
+  ggplot2::labs(x = "Salinity (g/L)") +
+  coord_flip()
 
-# ---- Join plots in one image ----
-ggplot_POMET_haline_zones_seine <- plot_POMET_map_seine + plot_POMET_salinity_zones_seine +
-  plot_layout(widths = c(4, 1), heights = c(1, 2))
+ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/plot_POMET_salinity_zones_seine.jpg",
+       width = 7, height = 7, units = "cm", dpi = 300,
+       plot = plot_POMET_salinity_zones_seine)
 
-# ----- save the image -----
+
+# ---- Join images with {cowplot} ----
+
+ggplot_POMET_haline_zones_seine <- plot_grid(
+  ggdraw() + draw_image("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_map_seine.jpg"),
+  ggdraw() + draw_image("inst/mat_meth/maps/POMET/haline_zones/plot_POMET_salinity_zones_seine.jpg"),
+  ncol = 2,
+  rel_widths = c(1.5, 1)
+)
 ggsave(filename = "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_seine.jpg",
        plot = ggplot_POMET_haline_zones_seine)
 
-# ----- crop the image -----
 img_seine <- image_read("inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_seine.jpg")
-img_trim_seine <- image_trim(img_seine)
+img_trim_seine <- image_trim(img_seine) # crop the image
 image_write(img_trim_seine, "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_seine.jpg")
 
+
+# =====================================================
+# 04. Join Gironde, Loire, Seine haline zones from POMET
+# =====================================================
+
+
+
+ggplot_POMET_haline_zones_estuaries <- plot_grid(
+  img("inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_gironde.jpg"),
+  img("inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_loire.jpg"),
+  img("inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_seine.jpg"),
+  ncol = 1,
+  align = "v"
+)
+
+
+ggsave(
+  "inst/mat_meth/maps/POMET/haline_zones/ggplot_POMET_haline_zones_estuaries.png",
+  ggplot_POMET_haline_zones_estuaries,
+  width = 10,
+  height = 12,
+  dpi = 300
+)
